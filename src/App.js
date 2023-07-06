3a20d04aa1a0ef42bf5260a4e0de16ad870bb384
HenrietteDaughtyOloo <henriettedoloo@gmail.com>
2023-07-06 10:15:18 +0300

    Practiced using APIs

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Login from './Login';
+import Product from './Producs';
+
+function App() {
+  return (
+    <div>
+      <Login/>
+      <Product/>
+    </div>
+  );
+}
+
+export default App;
